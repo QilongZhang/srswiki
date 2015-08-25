f3bd950b75ab752d323c25544d9058d1f35f3703
winlin <winlin@vip.126.com>
2015-08-25 22:44:20 +0800

    update script for v3

--- replace.for.v2.v3.sh
+++ replace.for.v2.v3.sh
@@ -10,3 +10,13 @@ else
     for file in $files; do for key in $keys; do echo "process file $file for key $key"; sed -i "s/v1_CN_$key/v2_CN_$key/g" $file; done done
 fi
 
+files=`ls v3*`
+keys=`ls v3*|awk -F '_' '{print $3}'|awk -F '.' '{print $1}'`
+uname -s |grep "Darwin" >/dev/null 2>&1
+if [[ 0 -eq $? ]]; then
+    for file in $files; do for key in $keys; do echo "process file $file for key $key"; sed -i '' "s/v1_EN_$key/v3_EN_$key/g" $file; done done
+    for file in $files; do for key in $keys; do echo "process file $file for key $key"; sed -i '' "s/v1_CN_$key/v3_CN_$key/g" $file; done done
+else
+    for file in $files; do for key in $keys; do echo "process file $file for key $key"; sed -i "s/v1_EN_$key/v3_EN_$key/g" $file; done done
+    for file in $files; do for key in $keys; do echo "process file $file for key $key"; sed -i "s/v1_CN_$key/v3_CN_$key/g" $file; done done
+fi
