645bf55494cd13a78ec6f96f83c473d22e3efc22
winlin <winlin@vip.126.com>
2015-09-22 17:05:40 +0800

    refine script

--- correct.language.sh
+++ correct.language.sh
@@ -1,9 +1,24 @@
-sed -i '' "s/v1_EN_/v1_CN_/g" v1_CN_*
-sed -i '' "s/v2_EN_/v2_CN_/g" v2_CN_*
-sed -i '' "s/v3_EN_/v3_CN_/g" v3_CN_*
+#!/bin/bash
 
-sed -i '' "s/v1_CN_/v1_EN_/g" v1_EN_*
-sed -i '' "s/v2_CN_/v2_EN_/g" v2_EN_*
-sed -i '' "s/v3_CN_/v3_EN_/g" v3_EN_*
+uname -s |grep "Darwin" >/dev/null 2>&1
+if [[ 0 -eq $? ]]; then
+    SED="sed -i ''"
+else
+    SED="sed"
+fi
+echo "sed is $SED"
 
+files=`ls v*_CN_*` && for file in $files; do
+    $SED "s/v1_EN_/v1_CN_/g" $file
+done
+exit 0
+$SED "s/v2_EN_/v2_CN_/g" v*_CN_*
+$SED "s/v3_EN_/v3_CN_/g" v*_CN_*
+echo "all EN correct to CN"
 
+$SED "s/v1_CN_/v1_EN_/g" v*_EN_*
+$SED "s/v2_CN_/v2_EN_/g" v*_EN_*
+$SED "s/v3_CN_/v3_EN_/g" v*_EN_*
+echo "all CN correct to EN"
+
+echo "ok"
